a865f09ed29c6f74b7b22a29ed6d259c75ef8caf
Ango <ango.gomes@gmail.com>
2021-01-01 14:57:46 +0100

    grandpy answer management JS improvement

--- website/static/grandpy.js
+++ website/static/grandpy.js
@@ -1,19 +1,41 @@
-function initMap(coords) {
-    let map = new google.maps.Map(document.getElementById("map"), {
+function create_map(coords) {
+    var map_div = document.createElement("div");
+    map_div.setAttribute('id', 'map');
+
+    new google.maps.Map(map_div, {
         center: coords,
         zoom: 10,
     });
+
+    return map_div;
+}
+
+function create_element(name, text) {
+    var element = document.createElement(name);
+    var element_text = document.createTextNode(text);
+    element.appendChild(element_text);
+    return element;
 }
 
             
 $(document).ready(function(){
-
+    
     $('#ask_grandpy').click(function(e){
         e.preventDefault(); 
-
         var question = encodeURIComponent( $('#question').val() );
-        $('#sentence').append("<p>Moi " + question + "</p>");
-        $('#sentence').append("<p>Laisse moi réfléchir............</p>");
+        
+        // Sentence Div Creation
+        var chat_box = document.getElementById('chat-box');
+        var ask_box = document.createElement("div");
+        ask_box.setAttribute('id', 'sentence');
+        chat_box.appendChild(ask_box)
+
+
+        question_element = create_element("p", "Moi " + question)
+        grandpy_think_element = create_element("p", "Laisse moi réfléchir...")
+
+        ask_box.appendChild(question_element);
+        ask_box.appendChild(grandpy_think_element);
 
         $.ajax({
             url : "/ask_grandpy",
@@ -24,12 +46,22 @@ $(document).ready(function(){
             success: function(response, textStatus, jqXHR) {
                 var obj = JSON.parse(response);
                 alert(response);
-                $('#sentence').append("<p>GrandPy: Hey oui mon kiki j'ai la réponse! Elle se trouve juste ici!<a target='_blank' href=" + obj.url +">"+ obj.url +"</a></p>");
-                initMap(obj.location);
-                
+
+                grandpy_answer_element = create_element("p", "GrandPy: Hey oui mon kiki j'ai la réponse! Elle se trouve juste ici!");
+
+                var url = document.createElement("a");
+                var url_text = document.createTextNode(obj.url)
+                url.setAttribute('href', obj.url);
+                url.setAttribute('_target', 'blank');
+                url.appendChild(url_text);
+
+                grandpy_answer_element.appendChild(url);
+                ask_box.appendChild(grandpy_answer_element);
+                ask_box.appendChild(create_map(obj.location));
             },
             error: function (data) {
-                $('#sentence').append("<p>GrandPy: Je suis désolé mon kiki, Papy y sait pas tout tu sais :(</p>");
+                grandpy_answer_element = create_element("p", "GrandPy: Je suis désolé mon kiki, Papy y sait pas tout tu sais :(");
+                ask_box.appendChild(grandpy_answer_element);
             },
         });
     });
